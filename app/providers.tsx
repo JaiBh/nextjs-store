4f8478864d54814e35bbbcc718ecc5595a6b3c7e
Jai Bhullar <81278507+JaiBh@users.noreply.github.com>
2024-07-10 03:33:58 +0100

    clerk implementation

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,16 +1,20 @@
 "use client";
+import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from "./theme-providers";
 
 function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      {children}
-    </ThemeProvider>
+    <>
+      <Toaster></Toaster>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </>
   );
 }
 export default Providers;
